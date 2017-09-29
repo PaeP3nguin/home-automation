d3ac044e13c6aa48df26d9c464be3e24b5ea6ca7
WX <paep3nguin@gmail.com>
2017-09-29 00:01:06 -0700

    Test

---
+++ 433Utils-master/RPi_utils/codesend.h
@@ -0,0 +1,6 @@
+#ifndef CODESEND_H
+#define CODESEND_H
+
+int sendCode(int code, int pin, int pulseLength);
+
+#endif
---
+++ automate.cpp
@@ -0,0 +1,7 @@
+#include "./433Utils-master/RPi_utils/codesend.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	sendCode(4543804, 0, 180);
+}
