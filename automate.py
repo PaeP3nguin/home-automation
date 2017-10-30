50fc2132a2fcc3a88daf479d8a8c7605a79200de
WX <paep3nguin@gmail.com>
2017-10-30 00:07:14 -0700

    Argh receive doesn't want to work

--- automate.py
+++ automate.py
@@ -6,11 +6,14 @@ from multiprocessing import Process
 import Adafruit_GPIO.SPI as SPI
 import Adafruit_MCP3008
 import RPi.GPIO as GPIO
+import signal
 import time
+import sys
 
 PIR_PIN = 13
 PRESSURE_PIN = 18
 RF_TX_PIN = 21
+RF_RX_PIN = 20
 LIGHT_PIN = 27
 RF_PULSE_LENGTH = 180
 ON_CODE = 4543795
@@ -18,31 +21,32 @@ OFF_CODE = 4543804
 ALL_ON_CODE = 4551939
 ALL_OFF_CODE = 4551948
 
-RF = RFDevice(RF_TX_PIN)
-RF.enable_tx()
+RF_TX = RFDevice(RF_TX_PIN)
+RF_TX.enable_tx()
+
+RF_RX = RFDevice(RF_RX_PIN)
+# RF_RX.enable_rx()
 
 SPI_PORT = 0
 SPI_DEVICE = 0
-mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
 
 
 def main():
-    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    # listen_rf_proc = Process(target=listen_rf)
+    motion_process = Process(target=monitor_pressure)
+    # pressure_process = Process(target=monitor_pressure)
 
-    while True:
-        reading = GPIO.input(27)
-        print(reading)
-        time.sleep(0.10)
-        if reading:
-            RF.tx_code(ON_CODE, tx_pulselength=RF_PULSE_LENGTH)
-        else:
-            RF.tx_code(OFF_CODE, tx_pulselength=RF_PULSE_LENGTH)
-        # pressure.wait_for_pressure()
-        # print("PRESSURE_PRESSURE_PRESSURE_PRESSURE_PRESSURE")
-        # pressure.wait_for_no_pressure()
+    # listen_rf_proc.start()
+    motion_process.start()
+    # pressure_process.start()
+
+    # listen_rf_proc.join()
+    motion_process.join()
+    # pressure_process.join()
 
 
 def monitor_pressure():
+    mcp = Adafruit_MCP3008.MCP3008(spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE))
     while True:
         value = mcp.read_adc(6)
         if value >= 20:
@@ -53,14 +57,14 @@ def monitor_pressure():
 
 
 def monitor_light():
-    GPIO.setup(27, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+    GPIO.setup(LIGHT_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
     while True:
-        light = GPIO.input(27)
+        light = GPIO.input(LIGHT_PIN)
         print(light)
         if not light:
-            RF.tx_code(ON_CODE, tx_pulselength=RF_PULSE_LENGTH)
+            RF_TX.tx_code(ON_CODE, tx_pulselength=RF_PULSE_LENGTH)
         else:
-            RF.tx_code(OFF_CODE, tx_pulselength=RF_PULSE_LENGTH)
+            RF_TX.tx_code(OFF_CODE, tx_pulselength=RF_PULSE_LENGTH)
 
 
 def monitor_motion():
@@ -69,9 +73,20 @@ def monitor_motion():
     while True:
         GPIO.wait_for_edge(PIR_PIN, GPIO.RISING)
         print("You moved")
-        RF.tx_code(ON_CODE, tx_pulselength=RF_PULSE_LENGTH)
+        RF_TX.tx_code(ON_CODE, tx_pulselength=RF_PULSE_LENGTH)
         GPIO.wait_for_edge(PIR_PIN, GPIO.FALLING)
-        RF.tx_code(OFF_CODE, tx_pulselength=RF_PULSE_LENGTH)
+        RF_TX.tx_code(OFF_CODE, tx_pulselength=RF_PULSE_LENGTH)
+
+
+def listen_rf():
+    last_rf = None
+
+    while True:
+        if RF_RX.rx_code_timestamp != last_rf:
+            last_rf = RF_RX.rx_code_timestamp
+            print("code: {}, pulselength: {}, protocol: {}".format(
+                str(RF_RX.rx_code), str(RF_RX.rx_pulselength), str(RF_RX.rx_proto)))
+        time.sleep(0.25)
 
 
 def all_lights_on(delay=None):
@@ -88,31 +103,24 @@ def transmit_rf(code, delay=None):
     """Transmit an RF code"""
     if delay:
         time.sleep(delay)
-    RF.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
+    RF_TX.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
     time.sleep(0.10)
-    RF.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
+    RF_TX.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
     time.sleep(0.10)
-    RF.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
+    RF_TX.tx_code(code, tx_pulselength=RF_PULSE_LENGTH)
+
+
+def exithandler(signal, frame):
+    # These also call GPIO.cleanup()
+    RF_TX.cleanup()
+    RF_RX.cleanup()
+    sys.exit(0)
 
 
 if __name__ == '__main__':
     GPIO.setwarnings(False)
     GPIO.setmode(GPIO.BCM)
 
-    # pressure_process = Process(target=monitor_pressure)
-    # pressure_process.start()
+    signal.signal(signal.SIGINT, exithandler)
 
-    # pressure_process.join()
-
-    motion_process = Process(target=monitor_pressure)
-    motion_process.start()
-
-    motion_process.join()
-
-    # main()
-    # crain()
-    # GPIO.add_event_detect(PRESSURE_PIN, GPIO.BOTH, callback=high_pressure)
-    # while True:
-    # time.sleep(60)
-    # Also calls GPIO.cleanup()
-    RF.cleanup()
+    main()
